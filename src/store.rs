5101117fd3ab19ed333cee1eeae987dfaf3a23d4
Aziz Mukhtorjonov <aziz.mukhtorjonov@dsr-corporation.com>
2025-12-04 18:20:23 +0500

    ASDK-581: remove unnecessary method
    Signed-off-by: Aziz Mukhtorjonov <aziz.mukhtorjonov@dsr-corporation.com>

--- src/store.rs
+++ src/store.rs
@@ -212,11 +212,6 @@ impl Session {
         Self(inner)
     }
 
-    /// Getter for AnyBackendSession
-    pub fn inner(self) -> AnyBackendSession {
-        self.0
-    }
-
     /// Count the number of entries for a given record category
     pub async fn count(
         &mut self,
